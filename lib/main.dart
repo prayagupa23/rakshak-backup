95c654722a6f8eff3e5289d5eb3b2b6520558cba
Parth Salunke <144267660+sparth292@users.noreply.github.com>
2025-03-17 03:40:34 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,38 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:rakshak_backup_final/splashscreen.dart';
-import 'package:rakshak_backup_final/welcome_screen.dart';
-import 'package:rakshak_backup_final/Features/Feature1.dart'; // Import first feature screen
+import 'Features/Feature1.dart';
+import 'gender_detection/gender_detection.dart';
+import 'home_page.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
-  // Retrieve installation and login status
+  await Firebase.initializeApp(); // Firebase initialization
+  await setupCameras();
   final prefs = await SharedPreferences.getInstance();
-  final bool hasSeenFeatures = prefs.getBool('hasSeenFeatures') ?? false;
   final bool isLoggedIn = prefs.getBool('isLoggedIn') ?? false;
 
-  runApp(MyApp(hasSeenFeatures: hasSeenFeatures, isLoggedIn: isLoggedIn));
+  runApp(MyApp(isLoggedIn: isLoggedIn)); // Pass `isLoggedIn` correctly
 }
 
 class MyApp extends StatelessWidget {
-  final bool hasSeenFeatures;
   final bool isLoggedIn;
 
-  const MyApp({super.key, required this.hasSeenFeatures, required this.isLoggedIn});
+  const MyApp({super.key, required this.isLoggedIn});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Rakshak',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: hasSeenFeatures ? (isLoggedIn ? SplashScreen() : WelcomeScreen()) : Feature1screen(),
+      home: isLoggedIn ? Navbar() : const Feature1screen(),
     );
   }
 }
